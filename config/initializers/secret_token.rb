0a84b0b976ff65dd73eb4c3117726ee5ff8a901f
Sumayya <tabassumsumayya@rocketmail.com>
2013-02-23 18:31:53 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Summi::Application.config.secret_token = 'e1907d6bd8ddd222d954795f2f1a4fbe57dddbeef789e9c486da87c64e077c0a9beeab8bf1a004e40f32e2238b727bbed526c346ddf09078cf8a44c228435d86'
